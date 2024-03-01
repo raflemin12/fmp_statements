17e018e33e1727abd28cd0205c4fad08a28458d7
Ricky Fleming <raflemin@gmail.com>
2024-03-01 15:26:27 -0800

    create Statement class

--- data_retrieval.py
+++ data_retrieval.py
@@ -2,55 +2,77 @@ import requests
 import pandas as pd
 from fmp_api_key import API_KEY
 
-def get_financial_statement(ticker: str, api_key: str=API_KEY, period: str='annual',
-                            balance_sheet: bool=True,
-                            income_statement: bool=False,
-                            cashflow_statement: bool=False):
-    """
-    Retrieves statment data from fmp and returns json
-    """
-    payload = {'period': period, 'apikey': api_key}
-    BASE_URL = 'https://financialmodelingprep.com/api/v3/'
+class Statement:
+    def __init__(self, ticker: str, period: str='annual') -> None:
+        self.ticker = ticker
+        self.BASE_URL = 'https://financialmodelingprep.com/api/v3/'
+        self.__api_key = API_KEY
+        self.period = period
+        self.payload = {'period': self.period, 'apikey': self.__api_key}
 
-    def statement_type(balance: bool= balance_sheet,
-                       income: bool= income_statement,
-                       cashflow: bool= cashflow_statement) -> str:
+    def get_financial_statement_json(self, statement:str):
         """
-        Returns the appropriate string to retrieve the requested statement from fmp
+        Retrieves statment data from fmp and returns json
         """
-        if balance:
-            return 'balance-sheet-statement'
-        if income:
-            return 'income-statement'
-        if cashflow:
-            return 'cash-flow-statement'
-        return None
+        try:
+            url = f'{self.BASE_URL}{statement}/{self.ticker}?'
+            response = requests.get(url, params=self.payload, timeout= 5)
+            return response.json()
+        except Exception as exc:
+            print("Unable to retrieve data")
+            raise exc
 
-    statement = statement_type()
+    def json_to_dict(self, data_json) -> dict:
+        """
+        Transforms json data into a dict with datapoint name as the key
+        and the datapoint in a list for the dict value
+        """
+        statement_dict = {}
+        no_entry = ['symbol', 'cik', 'fillingDate', 'acceptedDate',
+                    'calendarYear', 'period', 'link', 'finalLink', 'reportedCurrency']
+        for json in data_json:
+            for key, value in json.items():
+                if key in statement_dict:
+                    statement_dict[key].insert(0,value)
+                elif key not in no_entry:
+                    statement_dict[key] = [value]
+        return statement_dict
 
-    try:
-        url = f'{BASE_URL}{statement}/{ticker}?'
-        response = requests.get(url, params=payload, timeout= 5)
-        return response.json()
-    except Exception as exc:
-        print("Unable to retrieve data")
-        raise exc
+    def horizontal_analysis(self, statement_dict:dict) -> dict:
+        """
+        Takes in the statement_dict from json_to_dict and returns
+        horizontal analysis in the same format
+        """
+        def percent_delta(new:float, old:float) -> float:
+            """
+            Returns the percentage change from two values
+            """
+            if old > 0:
+                return round((new - old) / old, 3)
+        horizon_dict = {}
+        for key, lst in statement_dict.items():
+            if key == 'date':
+                horizon_dict[key] = lst.copy()
+            else:
+                horizon_dict[key] = [percent_delta(lst[idx], lst[idx-1]) for idx in range(1, len(lst))]
+                horizon_dict[key].insert(0,0)
+        return horizon_dict
 
-def json_to_dict(data_json) -> dict:
-    """
-    Transforms json data into a dict with datapoint name as the key
-    and the datapoint in a list for the dict value
-    """
-    statement_dict = {}
-    no_entry = ['symbol', 'cik', 'fillingDate', 'acceptedDate',
-                'calendarYear', 'period', 'link', 'finalLink', 'reportedCurrency']
-    for json in data_json:
-        for key, value in json.items():
-            if key in statement_dict:
-                statement_dict[key].insert(0,value)
-            elif key not in no_entry:
-                statement_dict[key] = [value]
-    return statement_dict
+    def vert_analysis(self, statement_dict: dict) -> dict:
+        """
+        Performs vertical analysis on the given statement.
+        Returns the analysis in dict form
+        """
+        # need to distiguish the type of statement
+        # for balance sheet, give option of totalAssets or totalLiabilities
+        vert_dict = {}
+        for key, lst in statement_dict.items():
+            if key == 'date':
+                vert_dict[key] = lst.copy()
+            else:
+                vert_dict[key] = [round(lst[idx] / statement_dict['totalAssets'][idx], 3)
+                                for idx in range(len(lst))]
+        return vert_dict
 
 # Change data retrieval to an object
     # Statement Class
@@ -67,38 +89,3 @@ def json_to_dict(data_json) -> dict:
             # Balance Sheet
             # Income Statement
             # Cashflow Statement
-def horizontal_analysis(statement_dict:dict) -> dict:
-    """
-    Takes in the statement_dict from json_to_dict and returns
-    horizontal analysis in the same format
-    """
-    def percent_delta(new:float, old:float) -> float:
-        """
-        Returns the percentage change from two values
-        """
-        if old > 0:
-            return round((new - old) / old, 3)
-    horizon_dict = {}
-    for key, lst in statement_dict.items():
-        if key == 'date':
-            horizon_dict[key] = lst.copy()
-        else:
-            horizon_dict[key] = [percent_delta(lst[idx], lst[idx-1]) for idx in range(1, len(lst))]
-            horizon_dict[key].insert(0,0)
-    return horizon_dict
-
-def vert_analysis(statement_dict: dict) -> dict:
-    """
-    Performs vertical analysis on the given statement.
-    Returns the analysis in dict form
-    """
-    # need to distiguish the type of statement
-    # for balance sheet, give option of totalAssets or totalLiabilities
-    vert_dict = {}
-    for key, lst in statement_dict.items():
-        if key == 'date':
-            vert_dict[key] = lst.copy()
-        else:
-            vert_dict[key] = [round(lst[idx] / statement_dict['totalAssets'][idx], 3)
-                              for idx in range(len(lst))]
-    return vert_dict
