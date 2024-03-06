7d4e411cf627caccd465f3ae914a93cf08ea2973
Ricky Fleming <raflemin@gmail.com>
2024-03-06 15:55:26 -0800

    see if remove_data_index method functions properly

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from data_retrieval import *
 
 def main():
     nke_balance = BalanceSheet('NKE')
+    nke_balance.remove_date_index(nke_balance.statement_df)
     print(nke_balance.statement_df)
 
 if __name__ == '__main__':
